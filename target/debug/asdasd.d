e9e2b5d554109a64857acf51a88f54e6f1e22eeb
MarchMol <marchmol98@gmail.com>
2024-07-11 16:39:16 -0600

    initial commit

---
+++ target/debug/asdasd.d
@@ -0,0 +1,1 @@
+/home/march/semestre_6/lab1_fill_poly/target/debug/asdasd: /home/march/semestre_6/lab1_fill_poly/src/main.rs
---
+++ target/debug/deps/asdasd-0d955b979dcdb85f.d
@@ -0,0 +1,5 @@
+/home/march/semestre_6/lab1_fill_poly/target/debug/deps/asdasd-0d955b979dcdb85f: src/main.rs
+
+/home/march/semestre_6/lab1_fill_poly/target/debug/deps/asdasd-0d955b979dcdb85f.d: src/main.rs
+
+src/main.rs:
